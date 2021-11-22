391d06d0763b43a62866d3b9899256fdd92a8308
vtbano <92877260+vtbano@users.noreply.github.com>
2021-11-22 13:30:13 -0500

    Upload Ophie's Weather App
    Draft stages of HTML, CSS, JS.
    
    JS is connected to the API hosted by https://openweathermap.org/

---
+++ Ophie's Weather App-Week 5/src/weatherapp.js
@@ -0,0 +1,98 @@
+//Feature#1
+let now = new Date();
+
+let dateDetails = document.querySelector(".date-and-time");
+
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let month = months[now.getMonth()];
+
+dateDetails.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes} EST`;
+
+//Feature#2
+function searchInput(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#citySearch");
+  let showCityInput = document.querySelector(".city-typed");
+  showCityInput.innerHTML = `${cityInput.value.toUpperCase()}`;
+
+  let apiCity = cityInput.value;
+  let apiKey = "0ecc16a08b2cbf15e6cb6ef5e3476181";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${apiCity}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function showTemperature(response) {
+  let mainTemp = Math.round(response.data.main.temp);
+  let currentTemp = document.querySelector(".currentTemp");
+  currentTemp.innerHTML = `${mainTemp}°C`;
+  let mainWeatherDescript = response.data.weather[0].description;
+  let weatherdescription = document.querySelector(".currentTempDscrpt");
+  weatherdescription.innerHTML = mainWeatherDescript;
+  let currenthumidity = Math.round(response.data.main.humidity);
+  let humiditydescription = document.querySelector(".humidity");
+  humiditydescription.innerHTML = `Humidity: ${currenthumidity}%`;
+  let currentwind = Math.round(response.data.wind.speed);
+  let windDescription = document.querySelector(".wind");
+  windDescription.innerHTML = `Wind Speed: ${currentwind} mph`;
+}
+
+let button = document.querySelector(".search");
+button.addEventListener("click", searchInput);
+
+//Feature #3
+function pinCurrentPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "0ecc16a08b2cbf15e6cb6ef5e3476181";
+  let apiPinUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiPinUrl).then(showPinTemperature);
+}
+
+function showPinTemperature(response) {
+  let mainTemp = Math.round(response.data.main.temp);
+  let currentTemp = document.querySelector(".currentTemp");
+  currentTemp.innerHTML = `${mainTemp}°C`;
+  let mainWeatherDescript = response.data.weather[0].description;
+  let weatherdescription = document.querySelector(".currentTempDscrpt");
+  weatherdescription.innerHTML = mainWeatherDescript;
+  let currenthumidity = Math.round(response.data.main.humidity);
+  let humiditydescription = document.querySelector(".humidity");
+  humiditydescription.innerHTML = `Humidity: ${currenthumidity}%`;
+  let currentwind = Math.round(response.data.wind.speed);
+  let windDescription = document.querySelector(".wind");
+  windDescription.innerHTML = `Wind Speed: ${currentwind} mph`;
+}
+let pin = document.querySelector(".pin");
+pin.addEventListener("click", pinCurrentPosition);
+
+navigator.geolocation.getCurrentPosition(pinCurrentPosition);
